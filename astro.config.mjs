cf4b45ac670488023d28c5a9ce2d66d1ab2ed0de
yokoberek <teisandatu@gmail.com>
2025-02-11 11:38:31 +0900

    feat: add site URL and enhance meta tags for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dapoerpapoea.web.id",
   integrations: [tailwind()],
   output: 'server',
   adapter: cloudflare()
